490647d6b79cadce4ea1e2e2a7b1adb019a75fb3
Fred Feng <paganini.fy@gmail.com>
2025-01-01 09:22:59 +1100

    Update code

--- doodler-common-redis/src/main/java/com/github/doodler/common/redis/SharedLock.java
+++ doodler-common-redis/src/main/java/com/github/doodler/common/redis/SharedLock.java
@@ -1,21 +1,26 @@
-package com.github.doodler.common.annotations;
+package com.github.doodler.common.redis;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 
- * @Description: EnableSharedService
+ * @Description: SharedLock
  * @Author: Fred Feng
- * @Date: 24/12/2024
+ * @Date: 01/01/2025
  * @Version 1.0.0
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface EnableSharedService {
+public @interface SharedLock {
+
+    long duration() default 0;
+
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
 
 }
